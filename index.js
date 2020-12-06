931b534f9eacaf365b43f4ab465ad74e49f8fdc2
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2020-12-06 10:20:42 -0600

    45%

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const form = document.getElementById('form')
+const btn = document.getElementById('btn')
 
 form.addEventListener('submit', event => {
     event.preventDefault()
     console.log('This is a Log')
+    console.log(this)
+})
+
+btn.addEventListener('click', function () {
+    
 })
